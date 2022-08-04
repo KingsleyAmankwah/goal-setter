10dc8b6c5952a8a08952c9c3cd8286145a4464de
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-08-04 18:26:04 +0000

    Database setup

---
+++ dashboard.php
@@ -0,0 +1,20 @@
+<?php 
+
+include './config/db.php';
+session_start();
+if(!$_SESSION['user']) { header("Location: index.php"); }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1>You're welcome <?php echo $_SESSION['user']; ?></h1>
+    <a href="logout.php">logout</a>
+</body>
+</html>
