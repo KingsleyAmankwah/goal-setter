94074555575ef1dc0e89934b24fd88018ccd15e6
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-08-04 17:34:18 +0000

    Sweetalert js

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login System</title>
+</head>
+<body>
+    <div class="container">
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="">Email</label>
+                <input type="email" name="Email" class="form-control" placeholder="Enter Email">
+            </div>
+            <div class="form-group">
+                <label for="">Password</label>
+                <input type="email" name="Password" class="form-control" placeholder="Enter Password">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
